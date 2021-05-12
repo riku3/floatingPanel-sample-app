2d9a92aed87decaa86897023f3928b0e6bb01b5c
riku3 <takarakuten62@gmail.com>
2021-05-12 12:08:38 +0900

    半モーダルに表示用のテーブルを用意

---
+++ floatingPanel-sample-app/TableViewController.swift
@@ -0,0 +1,25 @@
+//
+//  TableViewController.swift
+//  floatingPanel-sample-app
+//
+//  Created by riku on 2021/05/12.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        return cell
+    }
+
+}
